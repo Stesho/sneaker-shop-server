9820438530d7156aa38c00b8622a7fa57477efa6
Steshenko Dmitriy <3301297@mail.ru>
2022-09-28 02:08:05 +0300

    feat: add routes and controller for user

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import corseMeddleware from './middleware/cors.middleware.js';
+import userRouter from './routers/user.router.js';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(corseMeddleware);
 app.use(express.json());
+
+app.use('/api', userRouter);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
