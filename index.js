d3fa14742f6731df69fa9a3f4be0542f16f6f058
Steshenko Dmitriy <3301297@mail.ru>
2022-10-01 03:21:17 +0300

    feat: add user registration

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import corseMeddleware from './middleware/cors.middleware.js';
-import userRouter from './routers/user.router.js';
+import corseMeddleware from './middleware/corsMiddleware.js';
+import userRouter from './routers/userRouter.js';
 
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +9,6 @@ const app = express();
 app.use(corseMeddleware);
 app.use(express.json());
 
-app.use('/api', userRouter);
+app.use('/auth', userRouter);
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
